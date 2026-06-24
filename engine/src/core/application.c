e7e9bc832d8395b761e2092d8a51b87d8356d09d
Lan27Man <duane_rhodes2006@hotmail.com>
2026-06-24 23:04:55 +1000

    Memory Subsysten
    Completed Tutorial 007.

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -2,6 +2,7 @@
 #include "game_types.h"
 #include "logger.h"
 #include "platform/platform.h"
+#include "core/kmemory.h"
 
 typedef struct application_state
 {
@@ -69,6 +70,8 @@ b8 application_create(game* game_inst)
 
 b8 application_run()
 {
+    KINFO(get_memory_usage_str());
+
     while (app_state.is_running)
     {
         if (!platform_pump_messages(&app_state.platform))
